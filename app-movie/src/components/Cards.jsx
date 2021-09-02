5175fdbcddf59a70dec8d18c10e87bc17311e559
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-09-01 21:12:15 -0500

    creando aplicacion

---
+++ app-movie/src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Cards extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Tarjeta de pelis</h1>
+            </div>
+        )
+    }
+}
+
+
+
---
+++ app-movie/src/containers/ListContainer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Cards from '../components/Cards'
+export default class listContainer extends Component {
+    render() {
+        return (
+            <div>
+                <Cards/>
+            </div>
+        )
+    }
+}
